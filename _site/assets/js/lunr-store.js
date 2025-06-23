b4aa0c5c0e43ac5eea900896b297e1a142b75666
Do Ryu <dyryu@live.com>
2025-06-23 14:18:33 -0400

    corrected metadata for key artifacts

--- _site/assets/js/lunr-store.js
+++ _site/assets/js/lunr-store.js
@@ -2,4 +2,32 @@ var store = [{
 "title": "LibLab Bear","date": "2024","creator": "AF", 
 "id": "1.html"
 
+},{  
+"title": "Peace Key from Reading, Pennsylvania","date": "2025","creator": "Do Ryu", 
+"id": "8.html"
+
+},{  
+"title": "Peace Key from Elkhart, Indiana","date": "2025","creator": "Eve Umutoni", 
+"id": "10.html"
+
+},{  
+"title": "Peace Key from Perrysburg, Ohio","date": "2025","creator": "RR", 
+"id": "12.html"
+
+},{  
+"title": "Peace Key from Ann Arbor, Michigan","date": "2025","creator": "Do Ryu", 
+"id": "14.html"
+
+},{  
+"title": "\"Liberty\" Printing Block","creator": "DR", 
+"id": "16.html"
+
+},{  
+"title": "Iraq War Protest Bus (Front)","creator": "DR", 
+"id": "17.html"
+
+},{  
+"title": "Peace Key (Wooden)","creator": "DR", 
+"id": "19.html"
+
 }];
